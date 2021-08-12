94bcf1fd1db2a06f5c9e17a77feafc4cdabd957c
kshitij22999 <kshitij22999@gmail.com>
2021-08-12 16:26:27 +0530

    userentit

---
+++ src/main/java/com/training/Sprint1/entities/TripBoolking.java
@@ -0,0 +1,5 @@
+package com.training.Sprint1.entities;
+
+public class TripBoolking {
+	
+}
---
+++ src/main/java/com/training/Sprint1/entities/User.java
@@ -0,0 +1,72 @@
+package com.training.Sprint1.entities;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+
+@MappedSuperclass
+@Table(name="cba_user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String username;
+	private String password;
+	private String mobileNumber;
+	private String email;
+	private String address;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public User() {
+		super();
+	}
+
+	public User(String username, String password, String mobileNumber, String email, String address) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.address = address;
+	}
+
+}
