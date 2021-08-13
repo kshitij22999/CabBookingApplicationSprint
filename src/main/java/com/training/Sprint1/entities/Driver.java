d60e3cfab20a9512444a9021bda1c470fae87667
shekharkola8 <87413831+shekharkola8@users.noreply.github.com>
2021-08-13 12:45:05 +0530

    Abc

--- src/main/java/com/training/Sprint1/entities/Driver.java
+++ src/main/java/com/training/Sprint1/entities/Driver.java
@@ -3,9 +3,6 @@ package com.training.Sprint1.entities;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -13,11 +10,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="cba_driver")
-public class Driver { 
+public class Driver extends User{ 
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long driverId;
 	private String driverName;
 	private String lisenceNo;
 	private float rating;
@@ -39,10 +33,10 @@ public class Driver {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Driver(Long driverId, String driverName, String lisenceNo, float rating, Cab cab, TripBooking tripbooking,
+
+	public Driver(long id, String driverName, String lisenceNo, float rating, Cab cab, TripBooking tripbooking,
 			VaccinationStatus vaccinationStatus, AvailabilityStatus availabilityStatus) {
-		super();
-		this.driverId = driverId;
+		super(id);
 		this.driverName = driverName;
 		this.lisenceNo = lisenceNo;
 		this.rating = rating;
@@ -52,113 +46,114 @@ public class Driver {
 		this.availabilityStatus = availabilityStatus;
 	}
 
-	public Driver(String driverName, String lisenceNo, float rating, Cab cab, TripBooking tripbooking,
-			VaccinationStatus vaccinationStatus, AvailabilityStatus availabilityStatus) {
-		super();
-		this.driverName = driverName;
-		this.lisenceNo = lisenceNo;
-		this.rating = rating;
-		this.cab = cab;
-		this.tripbooking = tripbooking;
-		this.vaccinationStatus = vaccinationStatus;
-		this.availabilityStatus = availabilityStatus;
-	}
 
-	public Driver(String driverName, float rating, Cab cab, VaccinationStatus vaccinationStatus,
+	public Driver(String driverName, String lisenceNo, float rating, VaccinationStatus vaccinationStatus,
 			AvailabilityStatus availabilityStatus) {
 		super();
 		this.driverName = driverName;
+		this.lisenceNo = lisenceNo;
 		this.rating = rating;
-		this.cab = cab;
 		this.vaccinationStatus = vaccinationStatus;
 		this.availabilityStatus = availabilityStatus;
 	}
 
-	public Driver(Long driverId, String driverName, float rating) {
+
+	public Driver(String driverName, String lisenceNo, Cab cab) {
 		super();
-		this.driverId = driverId;
 		this.driverName = driverName;
-		this.rating = rating;
-	}
-	
-	
-
-	public Long getDriverId() {
-		return driverId;
+		this.lisenceNo = lisenceNo;
+		this.cab = cab;
 	}
 
-	public void setDriverId(Long driverId) {
-		this.driverId = driverId;
-	}
 
 	public String getDriverName() {
 		return driverName;
 	}
 
+
 	public void setDriverName(String driverName) {
 		this.driverName = driverName;
 	}
+
 
 	public String getLisenceNo() {
 		return lisenceNo;
 	}
 
+
 	public void setLisenceNo(String lisenceNo) {
 		this.lisenceNo = lisenceNo;
 	}
+
 
 	public float getRating() {
 		return rating;
 	}
 
+
 	public void setRating(float rating) {
 		this.rating = rating;
 	}
+
 
 	public Cab getCab() {
 		return cab;
 	}
 
+
 	public void setCab(Cab cab) {
 		this.cab = cab;
 	}
+
 
 	public TripBooking getTripbooking() {
 		return tripbooking;
 	}
 
+
 	public void setTripbooking(TripBooking tripbooking) {
 		this.tripbooking = tripbooking;
 	}
+
 
 	public VaccinationStatus getVaccinationStatus() {
 		return vaccinationStatus;
 	}
 
+
 	public void setVaccinationStatus(VaccinationStatus vaccinationStatus) {
 		this.vaccinationStatus = vaccinationStatus;
 	}
+
 
 	public AvailabilityStatus getAvailabilityStatus() {
 		return availabilityStatus;
 	}
 
+
 	public void setAvailabilityStatus(AvailabilityStatus availabilityStatus) {
 		this.availabilityStatus = availabilityStatus;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Driver [driverId=" + driverId + ", driverName=" + driverName + ", lisenceNo=" + lisenceNo + ", rating="
-				+ rating + ", cab=" + cab + ", tripbooking=" + tripbooking + ", vaccinationStatus=" + vaccinationStatus
-				+ ", availabilityStatus=" + availabilityStatus + "]";
+		return "Driver [driverName=" + driverName + ", lisenceNo=" + lisenceNo + ", rating=" + rating + ", cab=" + cab
+				+ ", tripbooking=" + tripbooking + ", vaccinationStatus=" + vaccinationStatus + ", availabilityStatus="
+				+ availabilityStatus + "]";
 	}
 
+
+
+	
+	
+}	
+
+
 	
 
 	
 	
+
 	
 	
-	
-}
