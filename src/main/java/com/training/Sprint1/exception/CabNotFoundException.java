53ac44edcecd0fffa30d458e05db50447062bd8f
Shinde-P <87411581+Shinde-P@users.noreply.github.com>
2021-08-16 13:09:09 +0530

    Add files via upload

---
+++ src/main/java/com/training/Sprint1/exception/CabNotFoundException.java
@@ -0,0 +1,15 @@
+package com.training.Sprint1.exception;
+
+public class CabNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	   
+
+	public CabNotFoundException() {
+
+	}
+
+	public CabNotFoundException(String message) {
+		super();
+	}
+}
