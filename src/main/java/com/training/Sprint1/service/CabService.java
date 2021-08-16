56d1a89aff5bc67eb280910779368498c2ac65e5
Sanika Bhagwat <53481795+SanikaBhagwat@users.noreply.github.com>
2021-08-16 12:47:11 +0530

    Add files via upload

--- src/main/java/com/training/Sprint1/service/CabService.java
+++ src/main/java/com/training/Sprint1/service/CabService.java
@@ -1,63 +1,59 @@
 package com.training.Sprint1.service;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.training.Sprint1.entities.Cab;
-import com.training.Sprint1.entities.CarType;
+import com.training.Sprint1.entities.carType;
 import com.training.Sprint1.repository.ICabRepository;
 import com.training.Sprint1.service.ICabService;
 
 
-
 @Service
 public class CabService implements ICabService {
+	@Autowired
+	ICabRepository iCabRepository;
 
 	@Override
 	public Cab insertCab(Cab cab) {
-		// TODO Auto-generated method stub
-		return null;
+		return iCabRepository.save(cab);
 	}
 
-	@Override
+	@Override 
 	public Cab updateCab(Cab cab) {
-		// TODO Auto-generated method stub
-		return null;
+		Cab obj = iCabRepository.findById(cab.getCabId()).get();
+		if (obj != null) {
+			obj.setCarType(cab.getCarType());
+			obj.setPerKmRate(cab.getPerKmRate());
+		}
+		return obj;
+		
 	}
 
 	@Override
 	public Cab deleteCab(String cabId) {
-		// TODO Auto-generated method stub
+	//	return iCabRepository.deleteById(cabId);
 		return null;
 	}
 
 	@Override
-
-	public List<Cab> viewCabsOfType(CarType carType) {
-
-		// TODO Auto-generated method stub
+	public List<Cab> viewCabsOfType(String cabId) {
+	//	return iCabRepository.findById(cabId);
 		return null;
 	}
 
 	@Override
-	public int countCabsOfType(CarType carType) {
-
+	public int countCabsOfType(carType carType) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 	@Override
 	public List<Cab> getAllCabs() {
-		// TODO Auto-generated method stub
-		return null;
+		return iCabRepository.findAll();
 	}
 
 
-
-
+	
 
 }
-
-
-
--- src/main/java/com/training/Sprint1/service/ICabService.java
+++ src/main/java/com/training/Sprint1/service/ICabService.java
@@ -1,22 +1,20 @@
 package com.training.Sprint1.service;
-
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.training.Sprint1.entities.Cab;
-import com.training.Sprint1.entities.CarType;
-
-
+import com.training.Sprint1.entities.carType;
 
 @Service
 
 public interface ICabService {
 	public Cab insertCab(Cab cab);
+	//public List<Cab> addAll(List<Cab> cabList);
 	public Cab updateCab(Cab cab);
 	public Cab deleteCab(String cabId);
-
-	public List<Cab> viewCabsOfType(CarType carType);
-	public int countCabsOfType(CarType carType);
+	public List<Cab> viewCabsOfType(String cabId);
 	public List<Cab> getAllCabs();
+	public int countCabsOfType(carType carType);
+
 }
