fd846fb8189acdcaf6192df15490b7307b035a34
Shinde-P <87411581+Shinde-P@users.noreply.github.com>
2021-08-16 13:29:14 +0530

    Add files via upload

--- src/main/java/com/training/Sprint1/repository/IDriverRepository.java
+++ src/main/java/com/training/Sprint1/repository/IDriverRepository.java
@@ -1,6 +1,7 @@
 package com.training.Sprint1.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,8 +9,10 @@ import org.springframework.data.jpa.repository.Query;
 import com.training.Sprint1.entities.Driver;
 
 public interface IDriverRepository extends JpaRepository<Driver ,Long> {
-	
+
+	Optional<Driver> getDriverById(Long id);
 	@Query(value = "from Driver dri where dri.getRating() >= 4.5")
 	public List<Driver> getBestDrivers();
+	Optional<Driver> findByDriverId(Long id);
 
 }
