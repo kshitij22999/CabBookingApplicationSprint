206ecda1b42674ea8c2df3f9200f7b26942dd175
Sanika Bhagwat <53481795+SanikaBhagwat@users.noreply.github.com>
2021-08-16 14:23:50 +0530

    Add files via upload

--- src/main/java/com/training/Sprint1/repository/ICabRepository.java
+++ src/main/java/com/training/Sprint1/repository/ICabRepository.java
@@ -1,16 +1,12 @@
 package com.training.Sprint1.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.training.Sprint1.entities.Cab;
-import com.training.Sprint1.entities.Driver;
 
 public interface ICabRepository  extends JpaRepository<Cab ,Long>  {
-
-	Optional<Cab> getCabById(String cabId);
-
-	Optional<Cab> findByCabId(String cabId);
+	
 
 	
+	
+
 }
